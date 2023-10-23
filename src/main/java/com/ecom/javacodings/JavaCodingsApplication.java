17ebf82c3475dc4c12081c6f5dd8611aa563c10b
Sentiomnen <sentiomnen@gmail.com>
2023-10-23 22:29:52 +0900

    스프링 부트 초기 설정

---
+++ src/main/java/com/ecom/javacodings/JavaCodingsApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.javacodings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/javacodings/JavaCodingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.javacodings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
