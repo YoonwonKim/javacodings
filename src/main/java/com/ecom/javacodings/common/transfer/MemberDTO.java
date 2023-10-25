0d06536eed54efa183c7bb6bfdd71cec5d539ce7
Sentiomnen <sentiomnen@gmail.com>
2023-10-25 10:53:01 +0900

    ADD Data Annotation

--- src/main/java/com/ecom/javacodings/common/transfer/MemberDTO.java
+++ src/main/java/com/ecom/javacodings/common/transfer/MemberDTO.java
@@ -1,7 +1,17 @@
 package com.ecom.javacodings.common.transfer;
 
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
 import java.sql.Date;
 
+@Data
+@Scope( value= WebApplicationContext.SCOPE_SESSION,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
+@ToString
 public class MemberDTO {
     String member_id;
     String password;
