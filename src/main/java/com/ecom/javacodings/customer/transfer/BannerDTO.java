0e50a3e8e5d991684ed75d520614277bc48c8cf7
KimYoonWon <yoonwon96@naver.com>
2023-10-30 11:37:41 +0900

    bannerDTO up
    up

---
+++ src/main/java/com/ecom/javacodings/customer/transfer/BannerDTO.java
@@ -0,0 +1,16 @@
+package com.ecom.javacodings.customer.transfer;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class BannerDTO {
+	  private String banner_id;
+	  private String item_id;
+      private String category;
+	  private String label;
+	  private String desc;
+	  private String image;
+	  private int reg_date;
+}
