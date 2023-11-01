bdb10250e738e3758d8285bddfa4d65603a04e9c
KimYoonWon <yoonwon96@naver.com>
2023-11-01 11:39:49 +0900

    Create TagDTO.java

---
+++ src/main/java/com/ecom/javacodings/customer/transfer/TagDTO.java
@@ -0,0 +1,14 @@
+package com.ecom.javacodings.customer.transfer;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class TagDTO {
+	
+	private String tag_id  ;
+	private String label   ;
+	private String desc    ;
+
+}
