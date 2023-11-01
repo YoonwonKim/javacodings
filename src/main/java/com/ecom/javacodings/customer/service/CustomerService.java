f6b6795a0427cc33ab810b1074c775399815899f
KimYoonWon <yoonwon96@naver.com>
2023-11-01 11:41:31 +0900

    MODIFY CustomerService
    커스텀서비스 추가

--- src/main/java/com/ecom/javacodings/customer/service/CustomerService.java
+++ src/main/java/com/ecom/javacodings/customer/service/CustomerService.java
@@ -1,9 +1,31 @@
 package com.ecom.javacodings.customer.service;
 
-import com.ecom.javacodings.common.transfer.MemberDTO;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+import com.ecom.javacodings.common.transfer.MemberDTO;
+import com.ecom.javacodings.customer.transfer.BannerDTO;
+import com.ecom.javacodings.customer.transfer.ItemDTO;
+import com.ecom.javacodings.customer.transfer.TagDTO;
+
+@Service
 public interface CustomerService {
     // Region 회원 정보 관리 메소드
     MemberDTO login(MemberDTO member);
     // End Region 회원 정보 관리 메소드
+    
+    List<BannerDTO> getbanner(int number);
+    
+    List<BannerDTO> listEvent(int number);
+
+	List<ItemDTO> listNew(ItemDTO item);
+	
+	List<ItemDTO> listBest(ItemDTO item);
+
+	Map<String, Object> listTagById(String tagId);
+	
+	
+
 }
