41efba7af4bd2a51d9d717a9ab9a6881c9ffa878
KimYoonWon <yoonwon96@naver.com>
2023-11-01 11:41:07 +0900

    MODIFY MemberService
    멤버 서비스 추가

--- src/main/java/com/ecom/javacodings/customer/service/MemberService.java
+++ src/main/java/com/ecom/javacodings/customer/service/MemberService.java
@@ -2,6 +2,11 @@ package com.ecom.javacodings.customer.service;
 
 import com.ecom.javacodings.common.transfer.MemberDTO;
 import com.ecom.javacodings.customer.access.MemberDAO;
+import com.ecom.javacodings.customer.transfer.BannerDTO;
+import com.ecom.javacodings.customer.transfer.ItemDTO;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +19,16 @@ public class MemberService implements CustomerService {
     public MemberDTO login(MemberDTO member) {
         return memberDAO.login(member);
     }
+
+	@Override
+	public BannerDTO getbanner(BannerDTO banner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BannerDTO> EventList(BannerDTO banner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
