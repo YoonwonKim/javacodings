a6ce9332f78f8651751a0c8891c9984b0e856a80
Sentiomnen <sentiomnen@gmail.com>
2023-11-03 16:46:18 +0900

    Create MerchandiserController 상품 관리자용 페이지 컨트롤러 생성 전체 상품 관리 페이지 메소드 개발 상품 정보를 JSON 형태로 전달하는 메소드 개발

---
+++ src/main/java/com/ecom/javacodings/merchandiser/controller/MerchandiserController.java
@@ -0,0 +1,53 @@
+package com.ecom.javacodings.merchandiser.controller;
+
+import com.ecom.javacodings.common.transfer.PageDTO;
+import com.ecom.javacodings.common.transfer.ItemDTO;
+import com.ecom.javacodings.merchandiser.service.ManagerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.http.HttpRequest;
+
+@Controller
+@RequestMapping("/manager/")
+public class MerchandiserController {
+    @Autowired
+    ManagerService managerService;
+
+    @RequestMapping("/products")
+    public String products(HttpServletRequest request, HttpServletResponse response,
+                           Model model) {
+        PageDTO page = new PageDTO();
+        page.setStart(0);
+        page.setRow(15);
+        page.setEnd(page.getRow() + page.getStart());
+        model.addAttribute("itemList", managerService.listItem(page));
+        model.addAttribute("categoryList", managerService.listCategory());
+
+        return "/merchandiser/products";
+    }
+
+    @PostMapping("/action/get_item")
+    @ResponseBody
+    public String getItem(HttpServletRequest request, HttpServletResponse response)
+            throws JsonProcessingException {
+        String item_id = request.getParameter("item_id");
+        System.out.println(item_id);
+        ItemDTO item = managerService.getItemById(item_id);
+        System.out.println(item);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writeValueAsString(item);
+        System.out.println(result);
+        return result;
+    }
+}
