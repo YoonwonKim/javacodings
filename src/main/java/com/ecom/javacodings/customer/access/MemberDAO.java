fc4d2375b6f80b4550a40ea64a8183297e587da0
zzi-mmi <miryeong0812@naver.com>
2023-11-03 10:24:24 +0900

    MODIFY MemberDAO
    회원가입 추가

--- src/main/java/com/ecom/javacodings/customer/access/MemberDAO.java
+++ src/main/java/com/ecom/javacodings/customer/access/MemberDAO.java
@@ -2,6 +2,8 @@ package com.ecom.javacodings.customer.access;
 
 import com.ecom.javacodings.common.transfer.MemberDTO;
 import com.ecom.javacodings.customer.transfer.BannerDTO;
+import com.ecom.javacodings.customer.transfer.ItemDTO;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 @Mapper
 public interface MemberDAO {
     MemberDTO login(MemberDTO member);
+    
+    int register(MemberDTO member);
+
+
 }
