cb901b6d025ba6148c6349f0946e7b172ff316f8
KimYoonWon <yoonwon96@naver.com>
2023-11-03 10:37:41 +0900

    MODIFIY ItemDAO

--- src/main/java/com/ecom/javacodings/customer/access/ItemDAO.java
+++ src/main/java/com/ecom/javacodings/customer/access/ItemDAO.java
@@ -9,7 +9,6 @@ import com.ecom.javacodings.customer.transfer.ItemDTO;
 
 @Mapper
 public interface ItemDAO {
-	String a();
 	List<ItemDTO> listNew(int number);
 	List<ItemDTO> listBest(int number);
 	List<ItemDTO> listItemsByTagId(String tagId);
@@ -17,7 +16,7 @@ public interface ItemDAO {
 	List<ItemDTO> listItem(int item);
 	int updateList(ItemDTO item);
 	int deleteItem(ItemDTO item);
-	
+	int updatePrice(ItemDTO item);
 	
 	
 }
