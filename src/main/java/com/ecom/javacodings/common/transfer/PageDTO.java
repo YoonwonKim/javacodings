94f426f0acd52e1a0295ad0bacded79768c1b30d
Sentiomnen <sentiomnen@gmail.com>
2023-11-03 16:41:38 +0900

    CREATE PageDTO

---
+++ src/main/java/com/ecom/javacodings/common/transfer/PageDTO.java
@@ -0,0 +1,12 @@
+package com.ecom.javacodings.common.transfer;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class PageDTO {
+    private int start;
+    private int row;
+    private int end;
+}
