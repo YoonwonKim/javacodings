2405a69042804f46b51d8dd64df5810c8233ce4f
Sentiomnen <sentiomnen@gmail.com>
2023-11-06 12:47:47 +0900

    MODIFY TagManagerDAO 특정 제품의 모든 태그 삭제 특정 제품의 모든 태그 추가

--- src/main/java/com/ecom/javacodings/merchandiser/access/TagManagerDAO.java
+++ src/main/java/com/ecom/javacodings/merchandiser/access/TagManagerDAO.java
@@ -4,9 +4,13 @@ import com.ecom.javacodings.common.transfer.TagDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface TagManagerDAO {
 	List<String> listTags();
 	List<TagDTO> listTagsById(String item_id);
+
+	int deleteTagsByItemId(String item_id);
+	int insertTags(Map<String, Object> params);
 }
