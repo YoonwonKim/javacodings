6974928ee815ab7ea5d79a28771a1bdbde6494a4
Sentiomnen <sentiomnen@gmail.com>
2023-11-06 11:02:05 +0900

    MODIFY TagManagerDAO 특정 아이템의 태그 쿼리

--- src/main/java/com/ecom/javacodings/merchandiser/access/TagManagerDAO.java
+++ src/main/java/com/ecom/javacodings/merchandiser/access/TagManagerDAO.java
@@ -1,5 +1,6 @@
 package com.ecom.javacodings.merchandiser.access;
 
+import com.ecom.javacodings.common.transfer.TagDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -7,4 +8,5 @@ import java.util.List;
 @Mapper
 public interface TagManagerDAO {
 	List<String> listTags();
+	List<TagDTO> listTagsById(String item_id);
 }
