6729cc7f63f9a3d067572728255c817385a50155
Sentiomnen <sentiomnen@gmail.com>
2023-11-07 16:39:06 +0900

    MODIFY Product management page
    상품 관리 페이지 수정,
    상품 정보 수정 용 모달 화면

--- src/main/webapp/resources/scripts/manage_products.js
+++ src/main/webapp/resources/scripts/manage_products.js
@@ -8,9 +8,14 @@ $().ready(() => {
             .querySelector('button');
         button.onclick = () => {
             id = item.getAttribute('item-id');
+            // setModal(id, 'data');
             setItemInfo(id);
         }
     }
+
+    $('cds-modal-footer-button#update').on('click', () => {
+        updateItem();
+    });
 })
 
 // Region Modal executor
@@ -19,21 +24,10 @@ function updateItem() {
 
     //? Get Item metadata
     let item = {};
-    item.label = modal
-        .querySelector('cds-text-input[name="label"]')
-        .getAttribute('value');
-    item.desc = modal
-        .querySelector('cds-text-input[name="desc"]')
-        .getAttribute('value');
-    item.price = modal
-        .querySelector('cds-number-input[name="price"]')
-        .getAttribute('value');
-    item.stock = modal
-        .querySelector('cds-number-input[name="stock"]')
-        .getAttribute('value');
-    item.category = modal
-        .querySelector('cds-select-item[selected]')
-        .getAttribute('value');
+    item.label = modal.querySelector('[name="label"]').getAttribute('value');
+    item.desc  = modal.querySelector('[name="desc" ]').getAttribute('value');
+    item.price = modal.querySelector('[name="price"]').getAttribute('value');
+    item.stock = modal.querySelector('[name="stock"]').getAttribute('value');
 
     //? Get Tags
     let tags = {};
@@ -43,7 +37,7 @@ function updateItem() {
 
     //! Send data to Database.
     $.ajax({
-        url: "/manager/action/set_item?item_id=" + id,
+        url: "/admin/actions/set_item?item_id=" + id,
         type: 'POST',
         dataType: 'json',
         data: Object.assign(item, tags),
@@ -55,11 +49,46 @@ function updateItem() {
 }
 // End Region Modal executor
 // Region Modal constructor
+function setModal(id, data) {
+    let modal = document.getElementById('item-modal');
+
+    let inputs = modal.querySelectorAll('.input');
+    for(let input of inputs) {
+        let category = input.getAttribute('category');
+        let name = input.getAttribute('name');
+        console.log(category, name);
+        input.setAttribute('value', data[category][name]);
+    }
+    // let image = modal.querySelector('img');
+    // let url = '/resources/images/';
+    // let category = image.getAttribute('category');
+    // image.setAttribute('src', url+data[category]['image']);
+
+    // modal.querySelector('cds-number-input[name="price"]')
+    //     .setAttribute('value', item.price);
+    // modal.querySelector('cds-number-input[name="stock"]')
+    //     .setAttribute('value', item.stock);
+    // modal.querySelector('cds-text-input[name="label"]')
+    //     .setAttribute('value', item.label);
+    // modal.querySelector('cds-text-input[name="desc"]')
+    //     .setAttribute('value', item.desc);
+    //
+    // modal.querySelector('cds-select-item[value="'+item.category+'"]')
+    //     .setAttribute('selected', true);
+
+    // modal.querySelector('cds-multi-select')
+    //     .setAttribute('value', tags);
+
+    // Open Modal
+    document.getElementById('item-modal').open = true;
+}
+
+
 function setItemInfo(id) {
     //? Get Item metadata from Database.
     let item;
     $.ajax({
-        url: "/manager/action/get_item?item_id=" + id,
+        url: "/admin/actions/get_item?item_id=" + id,
         async: false,
         type: 'POST',
         success: function(data) { item = JSON.parse(data); }
@@ -67,39 +96,15 @@ function setItemInfo(id) {
 
     let tags;
     $.ajax({
-        url: "/manager/action/get_tags?item_id=" + id,
+        url: "/admin/actions/get_tags?item_id=" + id,
         async: false,
         type: 'POST',
         success: function(data) { tags = JSON.parse(data); }
     });
 
-    console.log("TAGS : ", tags);
-    //? Input Item metadata into Modal.
-    let modal = document.getElementById('item-modal');
-    modal.querySelector('cds-modal-heading').innerHTML = item.label;
-    modal.querySelector('cds-modal-body-content').innerHTML = item.desc;
-    modal.querySelector('cds-number-input[name="price"]')
-        .setAttribute('value', item.price);
-    modal.querySelector('cds-number-input[name="stock"]')
-        .setAttribute('value', item.stock);
-    modal.querySelector('cds-text-input[name="label"]')
-        .setAttribute('value', item.label);
-    modal.querySelector('cds-text-input[name="desc"]')
-        .setAttribute('value', item.desc);
-    let image = modal.querySelector('img')
-    let url = '/resources/images/';
-    image.setAttribute('src', url+item.image);
-
-    modal.querySelector('cds-select-item[value="'+item.category+'"]')
-        .setAttribute('selected', true);
-
-    modal.querySelector('cds-multi-select')
-        .setAttribute('value', tags);
-
-    // Open Modal
-    document.getElementById('item-modal').open = true;
+    setModal(id, {item, tags});
 }
-function setModal() {
+function setMosdal() {
     let modal = document.getElementById('item-modal');
 
     modal.querySelector('cds-modal-heading').innerHTML = 'THIS';
