4dbd2a3bbbc9df381d081c5211547e26283c107f
KimYoonWon <yoonwon96@naver.com>
2023-11-09 17:24:16 +0900

    MODIFIY CONTROLLER

--- src/main/java/com/ecom/javacodings/customer/controller/PageController.java
+++ src/main/java/com/ecom/javacodings/customer/controller/PageController.java
@@ -1,5 +1,6 @@
 package com.ecom.javacodings.customer.controller;
 
+import com.ecom.javacodings.common.transfer.table.MemberDTO;
 import com.ecom.javacodings.customer.service.CustomerService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class PageController {
+	
     @Autowired
     CustomerService memberService;
 
@@ -37,4 +40,47 @@ public class PageController {
 
     	return "index";
     }
-}
+    
+    @RequestMapping("/join")
+    public String join(HttpServletRequest request, HttpServletResponse response,
+    					MemberDTO mdto, Model model) {
+    	
+    	return "customer/fragments/join";
+    }
+    
+    
+    
+    @RequestMapping("/registerProc")
+    public String registerProc(HttpServletRequest request,
+    							HttpServletResponse response,
+    							MemberDTO mdto, 
+    							Model model) {
+    	String phone = request.getParameter("phone1") + request.getParameter("phone2");
+    	mdto.setPhone(phone);
+    	
+    	
+    	try {
+			int r = memberService.memberJoin(mdto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+    	
+    	
+		return "redirect:/";
+	}
+    
+    @RequestMapping("/idCheck")
+	@ResponseBody
+	public int idCheck(HttpServletRequest request, 
+						HttpServletResponse response,
+						Model model,
+						MemberDTO mdto) {
+		int cnt = 0;	
+		  if(mdto.getMember_id()!=null) {
+			  cnt = memberService.idCheck(mdto.getMember_id());
+		  }
+		return cnt;
+	}
+    }
+
