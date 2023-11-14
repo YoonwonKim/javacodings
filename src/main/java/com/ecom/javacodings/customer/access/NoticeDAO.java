b062d504e534976b2f2ef9610bf489a0bf60c7d4
zzi-mmi <miryeong0812@naver.com>
2023-11-14 16:29:53 +0900

    MODIFY  NoticeMapper
    공지사항 추가

---
+++ src/main/java/com/ecom/javacodings/customer/access/NoticeDAO.java
@@ -0,0 +1,14 @@
+package com.ecom.javacodings.customer.access;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ecom.javacodings.common.transfer.PageDTO;
+import com.ecom.javacodings.common.transfer.table.NoticeDTO;
+
+@Mapper
+public interface NoticeDAO {
+	List<NoticeDTO> NoticeList(PageDTO page);
+	
+}
