7d66e088975dda078b99b569bd9cd57eb2aa2ad9
Sentiomnen <sentiomnen@gmail.com>
2023-11-21 16:46:27 +0900

    Refactoring
    주소 및 페이지 이름 변경

--- src/main/java/com/ecom/javacodings/customer/controller/PageController.java
+++ src/main/java/com/ecom/javacodings/customer/controller/PageController.java
@@ -64,58 +64,15 @@ public class PageController {
     					MemberDTO mdto, Model model) {
     	return "customer/account/register";
     }
-    
-    @RequestMapping("/registerProc")
-    public String registerProc(HttpServletRequest request,
-    							HttpServletResponse response,
-    							MemberDTO mdto, 
-    							Model model) throws ParseException {
-    	String phone = request.getParameter("phone1") + request.getParameter("phone2");
-    	String email = request.getParameter("email") + "@" + request.getParameter("email2");
-    	String birth = request.getParameter("birth1")  + "-" + request.getParameter("birth2")
-    	+ "-" + request.getParameter("birth3");
-    	
-    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-    	Date to = formatter.parse(birth);
-    	
-    	mdto.setPhone(Integer.parseInt(phone));
-    	mdto.setEmail(email);
-    	mdto.setBirth(to);
-    	
-    	System.out.println(to);
-    	try {
-			int r = memberService.memberJoin(mdto);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "redirect:/";
-	}
-    @RequestMapping("/idCheck")
-	@ResponseBody
-	public int idCheck(HttpServletRequest request, 
-						HttpServletResponse response,
-						Model model,
-						MemberDTO mdto) {
-		int cnt = 0;	
-		  if(mdto.getMember_id()!=null) {
-			  cnt = memberService.idCheck(mdto.getMember_id());
-		  }
-		return cnt;
-	}
 
-	@RequestMapping("account/search")
-	public String searchMember(HttpServletRequest request, HttpServletResponse response) {
-	  	
-		return "customer/searchmember";
-	}
-	
-	@RequestMapping("/memberinformation")
+	@RequestMapping("/account")
 	public String information(HttpServletRequest request, HttpServletResponse response, 
 						MemberDTO member, Model model) {
 		HttpSession session = request.getSession();
+		member = (MemberDTO) session.getAttribute("ssKey");
 		
 		model.addAttribute(member);
-		return "customer/account/memberinformation";
+		return "customer/account/information";
 	}
 }
 
