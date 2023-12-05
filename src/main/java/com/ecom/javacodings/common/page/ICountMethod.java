86242b64cd55bd3289444e6cc714224e72507086
Sentiomnen <sentiomnen@gmail.com>
2023-12-05 11:19:17 +0900

    Refactoring PageConstructor
    서비스 내부에서 재사용이 가능하도록 변경

---
+++ src/main/java/com/ecom/javacodings/common/page/ICountMethod.java
@@ -0,0 +1,6 @@
+package com.ecom.javacodings.common.page;
+
+@FunctionalInterface
+public interface ICountMethod {
+    int count(String criteria);
+}
--- src/main/java/com/ecom/javacodings/common/page/IListMethod.java
+++ src/main/java/com/ecom/javacodings/common/page/IListMethod.java
@@ -4,6 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 @FunctionalInterface
-public interface PageInterface {
-    List<Object> listObjects(Map<String, Object> params);
+public interface IListMethod {
+    List<Object> getList(String criteria, PageDTO page);
 }
